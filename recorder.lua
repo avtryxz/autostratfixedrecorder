64746ffb8fd4371cf7340ff4b807e49655b3ea3c
avtryxz <avtryxz@gmail.com>
2026-03-05 08:29:19 +0100

    Update recorder.lua

--- recorder.lua
+++ recorder.lua
@@ -723,7 +723,7 @@ return function(ctx)
 
                 if writefile then 
                     local config_header = string.format([[
-local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/avtryxz/autostratfixedrecorder/refs/heads/main/library.lua"))()
 
 TDS:Loadout("%s", "%s", "%s", "%s", "%s")
 TDS:Mode("%s")
